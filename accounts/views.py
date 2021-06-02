7147a9de917c7c27b68eea5fad0a35c9fc14648e
Shekhar088 <shekharsth2020@gmail.com>
2021-06-02 16:44:36 +0545

    Alert message and accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render,redirect
-
+from django.contrib import messages
 # Create your views here.
 
-def login(request):
-    return render(request, "accounts/login.html")
-
 def register(request):
+    if request.method =='POST':
+        messages.error(request, "Test Error message")
+        return redirect('register')
+        print("SUbmitted")
     return render(request, "accounts/register.html")
+
+
+def login(request):
+    if request.method == 'POST':
+        return
+    else:
+        return render(request, "accounts/login.html")
+
 
 def logout(request):
     return redirect('index')
